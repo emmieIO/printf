6a69ae06d2f4b7cb12245855f649f089fa3fc127
emmieIO <markonuoha6@gmail.com>
2023-07-23 19:43:28 +0100

    printf solution

--- get_precision.c
+++ get_precision.c
@@ -1,41 +1,38 @@
 #include "main.h"
+#include <ctype.h>
 
 /**
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * @i: Pointer to the current index in the format string.
+ * @list: List of arguments to be printed.
  *
- * Return: Precision.
+ * Return: Precision value or -1 if not specified or invalid.
  */
 int get_precision(const char *format, int *i, va_list list)
 {
-	int curr_i = *i + 1;
-	int precision = -1;
+int curr_i = *i + 1;
+int precision = -1;
 
-	if (format[curr_i] != '.')
-		return (precision);
+if (format[curr_i] != '.')
+return precision;
 
-	precision = 0;
+precision = 0;
+curr_i++;
 
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-	{
-		if (is_digit(format[curr_i]))
-		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			precision = va_arg(list, int);
-			break;
-		}
-		else
-			break;
-	}
+if (isdigit(format[curr_i])) {
+while (isdigit(format[curr_i])) {
+precision = precision * 10 + (format[curr_i] - '0');
+curr_i++;
+}
+} else if (format[curr_i] == '*') {
+curr_i++;
+precision = va_arg(list, int);
+} else {
+// If there is a dot without a numeric value or wildcard, consider precision as 0.
+precision = 0;
+}
 
-	*i = curr_i - 1;
-
-	return (precision);
+*i = curr_i - 1;
+return precision;
 }
