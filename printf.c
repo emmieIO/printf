56b9725c92d681a4d90fec0d01563579a8bb8af8
emmieIO <markonuoha6@gmail.com>
2023-07-24 14:41:20 +0100

    printf solution

--- printf.c
+++ printf.c
@@ -3,7 +3,7 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
+ * _printf - entry point of Printf function
  * @format: format.
  * Return: Printed chars.
  */
