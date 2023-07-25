1e5fab46b52a080beb9cb80b88825759df40b1b4
Bright Aboagye <brightkotos123@gmail.com>
2023-07-25 00:44:58 -0700

    finalizing get_precision

--- get_prec.c
+++ get_prec.c
@@ -14,8 +14,9 @@ int curr_i = *i + 1;
 int precision = -1;
 
 if (format[curr_i] != '.')
+{
 return (precision);
-
+}
 precision = 0;
 
 for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
@@ -32,11 +33,12 @@ precision = va_arg(list, int);
 break;
 }
 else
+{
 break;
+}
 }
 
 *i = curr_i - 1;
 
 return (precision);
 }
-
