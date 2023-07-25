45d9795148c9390af9d534969b44cc9efc5d155a
Bright Aboagye <brightkotos123@gmail.com>
2023-07-24 22:04:43 -0700

    refactoring the get precision function with a while loop

--- get_prec.c
+++ get_prec.c
@@ -1,9 +1,8 @@
 #include "main.h"
-#include <stdbool.h>
 
 /**
- * get_precision - Calculates the precision for printing.
- * @format: Formatted string in which to print the arguments.
+ * get_precision - Calculates the precision for printing
+ * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: List of arguments.
  *
@@ -14,26 +13,32 @@ int get_precision(const char *format, int *i, va_list list)
 int curr_i = *i + 1;
 int precision = -1;
 
-if (format == NULL || format[curr_i] != '.')
-return (precision);
+if (format[curr_i] != '.')
+return precision;
 
-curr_i++; // Move to the character after '.'.
-
-if (format[curr_i] == '*')
-{
-curr_i++; // Mov
-precision = va_arg(list, int);
-}
-else if (isdigit(format[curr_i]))
-{
 precision = 0;
-while (isdigit(format[curr_i]))
+curr_i++;
+
+while (format[curr_i] != '\0')
+{
+if (isdigit(format[curr_i]))
 {
 precision = precision * 10 + (format[curr_i] - '0');
 curr_i++;
 }
+else if (format[curr_i] == '*')
+{
+curr_i++;
+precision = va_arg(list, int);
+break;
+}
+else
+{
+break;
+}
 }
 
 *i = curr_i - 1;
-return (precision);
+return precision;
+}
 }
