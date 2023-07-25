72fa3ac1941a7c552eaf3e9d4d4d5eeff8a5ae72
Bright Aboagye <brightkotos123@gmail.com>
2023-07-24 22:48:02 -0700

    working on correcting the get precision

--- get_prec.c
+++ get_prec.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include <ctype.h>
+
 /**
- * get_precision - Calculates the precision for printing.
+ * get_precision - Extracts the precision value from the format string.
  * @format: Formatted string in which to print the arguments.
- * @i: List of arguments to be printed.
+ * @i: Pointer to the current index in the format string.
  * @list: List of arguments.
  *
- * Return: (Precision).
+ * Return: (Precision value).
  */
 int get_precision(const char *format, int *i, va_list list)
 {
@@ -20,23 +21,10 @@ precision = 0;
 curr_i++; /* Move to the character after '.'. */
 
 /* Extract the precision value using a while loop. */
-while (format[curr_i] != '\0')
-{
-if (isdigit(format[curr_i]))
+while (isdigit(format[curr_i]))
 {
 precision = precision * 10 + (format[curr_i] - '0');
 curr_i++;
-}
-else if (format[curr_i] == '*')
-{
-curr_i++; /* Move to the next character after '*'. */
-precision = va_arg(list, int);
-break;
-}
-else
-{
-break;
-}
 }
 
 *i = curr_i - 1;
